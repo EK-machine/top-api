f52ac1f8f447ac418a226961672680497986cb36
Yauhen Kalabukhin <coocooroozza@gmail.com>
2023-06-14 14:18:04 +0300

    models connections

--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TopPageController } from './top-page.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { TopPageModel } from './top-page.model';
 
 @Module({
+  imports: [TypegooseModule.forFeature([{
+    typegooseClass: TopPageModel,
+    schemaOptions: {
+      collection: 'TopPage'
+    }
+  }])],
   controllers: [TopPageController]
 })
 export class TopPageModule {}
