76c3399dacd97f57b36f5ab0fd67b7108c80b5a7
Yauhen Kalabukhin <coocooroozza@gmail.com>
2023-06-21 12:12:32 +0300

    added setting of JWT to auth service

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,1 +1,4 @@
 export const ALREADY_REGISTERED_ERROR = 'user with this login already exist';
+export const USER_NOT_FOUND_ERROR = 'user with this login does not exist';
+export const WRONG_PASSWORD_ERROR = 'the password is not correct';
+
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,14 +3,23 @@ import { AuthController } from './auth.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { UserModel } from './user.model';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJWTConfig } from 'src/configs/jwt.config';
 
 @Module({
-  imports: [TypegooseModule.forFeature([{
-    typegooseClass: UserModel,
-    schemaOptions: {
-      collection: 'User'
-    }
-  }])],
+  imports: [
+    TypegooseModule.forFeature([{
+      typegooseClass: UserModel,
+      schemaOptions: {
+        collection: 'User'
+      }
+    }]), JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJWTConfig
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
