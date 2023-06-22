c0313316964d8ac1b64ae9b77480c43bbe4fb5d2
Yauhen Kalabukhin <coocooroozza@gmail.com>
2023-06-22 11:47:58 +0300

    added product service and implemented in controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,31 +1,51 @@
-import { Controller, Post, Body, Get, Param, Patch, Delete, HttpCode } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch, Delete, HttpCode, NotFoundException, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ProductModel } from './product.model';
 import { FindProductDto } from './dto/find-product.dto';
+import { CreateProductDto } from './dto/create-product.dto';
+import { ProductService } from './product.service';
+import { PRODUCT_NOT_FOUND_ERROR } from './product.constants';
 
 @Controller('product')
 export class ProductController {
+	constructor(private readonly productService: ProductService) {
+
+	}
 
 	@Post('create')
-	async create(@Body() dto: Omit<ProductModel, '_id'>) {
-
+	async create(@Body() dto: CreateProductDto) {
+		return this.productService.create(dto);
 	}
 
 	@Get(':id')
 	async get(@Param('id') id: string) {
-
+		const product = await this.productService.findById(id);
+		if (!product) {
+			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+		}
+		return product;
 	}
 
 	@Delete(':id')
 	async delete(@Param('id') id: string) {
-
+		const deletedProduct = await this.productService.deleteById(id);
+		if (!deletedProduct) {
+			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+		}
 	}
 
 	@Patch(':id')
 	async patch(@Param('id') id: string, @Body() dto: ProductModel) {
-
+		const updatedproduct = await this.productService.updateById(id, dto);
+		if (!updatedproduct) {
+			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+		}
+		return updatedproduct;
 	}
 
+	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
-	@Post()
-	async find(@Body() dto: FindProductDto) { }
+	@Post('find')
+	async find(@Body() dto: FindProductDto) {
+		return this.productService.findWithReviews(dto);
+	 }
 }
